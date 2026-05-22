ab5781ae72221ae6d03f6d2ebdba9fa055535332
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-05-22 12:26:13 -0300

    dictionary to hangman module

--- files/hangman/lib/hangman.rb
+++ files/hangman/lib/hangman.rb
@@ -1,1 +1,2 @@
 require_relative './hangman/word'
+require_relative './hangman/dictionary'
--- files/hangman/spec/dictionary_spec.rb
+++ files/hangman/spec/dictionary_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/dictionary'
+require_relative '../lib/hangman'
 
 FILE_PATH = './files/google-10000-english-no-swears.txt'
 
@@ -6,12 +6,12 @@ describe "Testing dictionary" do
   let(:first_word) { File.foreach(FILE_PATH).first.chomp }
 
   it "Dictionary initialize should load words from the file" do
-    dic = Dictionary.new(FILE_PATH)
+    dic = Hangman::Dictionary.new(FILE_PATH)
     expect(dic.words).not_to be_empty
   end
 
   it "Dictionary should load words in a hash with the word length as key" do
-    dic = Dictionary.new(FILE_PATH)
+    dic = Hangman::Dictionary.new(FILE_PATH)
     expect(dic.words).to have_key(first_word.length)
     expect(dic.words[first_word.length]).to include(first_word)
   end
