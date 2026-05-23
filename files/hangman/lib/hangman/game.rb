3e9557a604a558c5f5b3e9e2eb596a63d576e735
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-05-23 19:32:24 -0300

    Game class

---
+++ files/hangman/lib/hangman/game.rb
@@ -0,0 +1,82 @@
+require_relative './word'
+
+module Hangman
+  class Game
+    TRIES = 6
+    attr_reader :words_played, :dictionary, :min_length, :max_length, :current_word
+
+    def initialize(dictionary, min_length = 5, max_length = 12)
+      @dictionary = dictionary
+      @words_played = []
+      @current_word = nil
+      set_limits(min_length, max_length)
+    end
+
+    def set_limits(min_length, max_length)
+      # Set word length limits.
+      @min_length = min_length
+      @max_length = max_length
+
+      # Set remaining words to be those within the limits.
+      @available_words = dictionary.words.select do |length, words|
+        length >= min_length && length <= max_length
+      end.values.flatten
+    end
+
+    def pick_word
+      # Can not pick a new word until the current word has been guessed correctly or revealed.
+      if self.current_word && (!self.current_word.guessed_correctly? && !self.current_word.revealed)
+        raise "Current word has not been guessed correctly yet."
+      end
+
+      # Check if there are any remaining words to pick from.
+      remaining_words = @available_words - self.words_played.map { |word| word.to_s }
+      if remaining_words.empty?
+        raise "No more words available within the specified length limits."
+      end
+      
+      # Set a new current word from the remaining ones.
+      self.current_word = Hangman::Word.new(@available_words.sample)
+    end
+
+    def guess_letter(letter)
+      # Word not picked.
+      if self.current_word.nil?
+        raise "No word has been picked yet."
+      end
+
+      # Already guessed.
+      if self.current_word.guessed_correctly? || self.current_word.revealed
+        raise "Curret word can not be guessed anymore. Please pick a new word."
+      end
+
+      # Already guessed this letter.
+      if self.current_word.guesses.include?(letter)
+        raise "Letter '#{letter}' has already been guessed. Please try a different letter."
+      end
+
+      self.current_word.guess(letter)
+
+      # Too many wrong guesses.
+      if self.current_word.wrong_guesses.length >= TRIES
+        self.current_word.revealed = true
+      end
+    end
+
+    def status
+      {
+        current_word: self.current_word ? self.current_word.to_s : nil,
+        guesses: self.current_word ? self.current_word.guesses : [],
+        wrong_guesses: self.current_word ? self.current_word.wrong_guesses : [],
+        words_played: self.words_played.filter do |word|
+          word.guessed_correctly? || word.revealed 
+        end.map { |word| word.to_s }
+      }
+    end
+
+    private def current_word=(word)
+      @current_word = word
+      @words_played << word
+    end
+  end
+end
---
+++ files/hangman/spec/game_spec.rb
@@ -0,0 +1,99 @@
+require_relative '../lib/hangman'
+
+def reveal_word(game)
+  letter = 'a'
+  Hangman::Game::TRIES.times { game.guess_letter(letter); letter = (letter.ord + 1).chr }
+end
+
+describe 'Testing Game' do
+  let(:dictionary) { Hangman::Dictionary.new('./files/google-10000-english-no-swears.txt') }
+  let(:game) { Hangman::Game.new(dictionary) }
+  
+  it 'Initializes with a dictionary and default limits' do
+    expect(game.dictionary).to eq(dictionary)
+    expect(game.words_played).to eq([])
+    expect(game.min_length).to eq(5)
+    expect(game.max_length).to eq(12)
+  end
+
+  describe 'It should let you set limits' do
+    it 'Should set the min and max length limits' do
+      game.set_limits(3, 8)
+      expect(game.min_length).to eq(3)
+      expect(game.max_length).to eq(8)
+    end
+  end
+
+  describe 'It should let pick a word' do
+    it 'Should pick a word within the specified limits' do
+      game.pick_word
+    end
+
+    it 'Should not pick a new word until the current one is guessed correctly' do
+      game.pick_word
+      expect { game.pick_word }.to raise_error(RuntimeError)
+    end
+  end
+
+  describe 'It should let guess a letter' do
+    it 'Should not let guess if no word has been picked' do
+      expect { game.guess_letter('a') }.to raise_error(RuntimeError)
+    end
+
+    it 'Should not let guess if the current word has already been guessed correctly' do
+      game.pick_word
+      game.guess_letter('a')
+      expect { game.guess_letter('a') }.to raise_error(RuntimeError)
+    end
+
+    it 'Should not let guess if the current word has already been guessed incorrectly too many times' do
+      game.pick_word
+
+      letter = 'a'
+      while !game.current_word.revealed
+        game.guess_letter(letter)
+        letter = (letter.ord + 1).chr
+      end
+
+      expect { game.guess_letter('a') }.to raise_error(RuntimeError)
+    end
+  end
+
+  describe 'It should return the game status' do
+    it 'Should return the current word, guesses, wrong guesses and words played' do
+      game.pick_word
+      status = game.status
+      expect(status).to have_key(:current_word)
+      expect(status).to have_key(:guesses)
+      expect(status).to have_key(:wrong_guesses)
+      expect(status).to have_key(:words_played)
+    end
+
+    it 'Current word should be nil if no word has been picked' do
+      expect(game.status[:current_word]).to be_nil
+    end
+
+    it 'Guesses and wrong guesses should be empty arrays if no word has been picked' do
+      expect(game.status[:guesses]).to be_empty
+      expect(game.status[:wrong_guesses]).to be_empty
+    end
+
+    describe 'Words played should be an empty array if...' do
+      it 'No word has been picked' do
+        expect(game.status[:words_played]).to be_empty
+      end
+
+      it 'The first word has been picked but not guessed correctly or revealed' do
+        game.pick_word
+        expect(game.status[:words_played]).to be_empty
+      end
+    end
+
+    it 'Words played should include the current word if it has been guessed correctly or revealed' do
+      game.pick_word
+      game.current_word.revealed = true
+      game.pick_word
+      expect(game.status[:words_played].length).to eq(1)
+    end
+  end
+end
